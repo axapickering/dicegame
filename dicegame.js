d0b883fca5d75e8332a0293c2e320e4e04ddc405
Axa Pickering <axapickering@gmail.com>
2024-01-17 12:44:40 -0800

    created base html/css/js files

---
+++ dicegame.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const scoreContainer = document.querySelector("#score-container");
+const rollTrackerContainer = document.querySelector("#roll-tracker-container");
+const diceBoardContainer = document.querySelector("#dice-board-container");
+
