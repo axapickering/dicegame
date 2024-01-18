7296f7b048f6c5f6353d8a1f414b1060cfd81e09
Axa Pickering <axapickering@gmail.com>
2024-01-18 10:37:22 -0800

    adjusted divs to their homes, added placeholder text

--- dicegame.js
+++ dicegame.js
@@ -4,3 +4,11 @@ const scoreContainer = document.querySelector("#score-container");
 const rollTrackerContainer = document.querySelector("#roll-tracker-container");
 const diceBoardContainer = document.querySelector("#dice-board-container");
 
+function rollDie() {
+  console.log("die rolled.")
+}
+
+diceBoardContainer.addEventListener("click",evt => {
+  evt.preventDefault();
+  rollDie();
+})
